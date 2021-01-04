0c38ce1632c7ef2ab58d2119edb982753cbf243f
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-04 11:02:25 +0700

    update TodoContext

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Policy;
 using System.Threading.Tasks;
+using TodoApi.Models.UserModels;
 
 namespace TodoApi.Models
 {
@@ -15,7 +16,7 @@ namespace TodoApi.Models
         }
 
         public DbSet<TodoItem> TodoItems { get; set; }
-
+        public DbSet<User> Users { get; set; }
    
             
     }
