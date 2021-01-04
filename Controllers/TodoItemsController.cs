60c0a2042c2f8262353fa88dce5b8487802ae057
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-04 20:42:23 +0700

    OK ! Can Authorize

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class TodoItemsController : ControllerBase
