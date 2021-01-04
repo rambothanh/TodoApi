879d046d60960b0c3d9f767900149660e4cc75fa
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-04 10:34:30 +0700

    add models

---
+++ Models/UserModels/UpdateModel.cs
@@ -0,0 +1,10 @@
+namespace TodoApi.Models.UserModels
+{
+  public class UpdateModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
