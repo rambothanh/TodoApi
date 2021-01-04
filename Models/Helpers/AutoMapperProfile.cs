2fbc980214f023c85b642e1a50ca474138f0cb1b
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-04 10:56:56 +0700

    add Auto Mapper

---
+++ Models/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using TodoApi.Models.UserModels;
+
+namespace TodoApi.Models.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<RegisterModel, User>();
+            CreateMap<UpdateModel, User>();
+        }
+    }
+}
