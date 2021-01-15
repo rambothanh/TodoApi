aa9f0248e00289eb423ad368d4c0a09d76283565
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-15 11:30:59 +0700

    Fix code

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -27,7 +27,7 @@ namespace TodoApi.Controllers
         [HttpGet]
         public async Task<ActionResult<List<News>>> GetNewss()
         {
-            //
+            //.AsSplitQuery() 
             return await _context.Newss
                             .Include(n => n.Category)
                             .Include(n => n.Content)
@@ -40,77 +40,22 @@ namespace TodoApi.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<News>> GetNews(long id)
         {
-            var news = await _context.Newss.FindAsync(id);
-
+            //var news = await _context.Newss.FindAsync(id);
+            var news = await _context.Newss
+                            .Include(n => n.Category)
+                            .Include(n => n.Content)
+                            .Include(n => n.ImageLink)
+                            .AsSplitQuery()
+                            .Where(n => n.Id ==id)
+                            .FirstAsync();
             if (news == null)
             {
                 return NotFound();
             }
-
+            
             return news;
         }
 
-        // PUT: api/News/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutNews(long id, News news)
-        {
-            if (id != news.Id)
-            {
-                return BadRequest();
-            }
 
-            _context.Entry(news).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!NewsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/News
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<News>> PostNews(News news)
-        {
-            _context.Newss.Add(news);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetNews", new { id = news.Id }, news);
-        }
-
-        // DELETE: api/News/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteNews(long id)
-        {
-            var news = await _context.Newss.FindAsync(id);
-            if (news == null)
-            {
-                return NotFound();
-            }
-
-            _context.Newss.Remove(news);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool NewsExists(long id)
-        {
-            return _context.Newss.Any(e => e.Id == id);
-        }
     }
 }
